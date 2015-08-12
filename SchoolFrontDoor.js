ed48e22397ce322d77ad69e868264817acab1902
1MmM1 <lin-family@live.com>
2015-08-12 12:51:47 -0700

    Added canvas switching function

--- SchoolFrontDoor.js
+++ SchoolFrontDoor.js
@@ -1,34 +1,30 @@
 //Setting up the canvas for the first scene
 /*var idList = ["#bigDoor", "mathClass"];
 var rmNum = 0;*/
-var gameOver = false;
 void setup() {
 	console.log("setup");
 	size(1000, 800);
-	background(0, 235, 255);
-	//$('#bigDoor').show();
+	background(0, 0, 255);
 }
 void draw(){
 	console.log("draw loop");
 	noLoop();
 }
-if (gameOver == true){
-	for (var g = 0; g < idList.lenth; g++){
-		$(idList[g]).hide();
-	}
-	$("#youLose").show();
-}
-/*void mousePressed(){
+//Mousepressed should be replaced with xy coordinates in the future
+//the xy coordinates should correspond to where the doors are in the background image
+void mousePressed(){
+	rmNum = 1;
 	console.log("where am I?");
 	noLoop();
 	//when the person hits a specific spot in the room, the rmNum changes
 	//when the rmNum changes, it shows the new room and hides all the other ones
 	$(idList[rmNum]).show();
 	for(var elem = 0; elem < idList.length; elem++){
+		console.log("for loop");
 		if (elem != rmNum){
 			$(idList[elem]).hide();
+			console.log("phwump!");
 		}
 	}
 
 }
-*/
