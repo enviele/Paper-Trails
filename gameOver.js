cad9304931870c3c1a0a8649d5629e893f3293bc
1MmM1 <lin-family@live.com>
2015-08-12 13:34:16 -0700

    Game over screen

--- gameOver.js
+++ gameOver.js
@@ -3,6 +3,8 @@ void setup() {
 	console.log("setup yourself to fail");
 	size(900, 800);
 	background(255, 0, 0);
+	congrats = loadImage("winScreen.jpg");
+	tooBad = loadImage("overScreen.jpg");
 	$("#overGame").hide();
 }
 if (gameOver == true){
@@ -13,3 +15,14 @@ if (gameOver == true){
 	//shows the game over/winning screen
 	$("#overGame").show();
 }
+/*void draw(){
+	if (gameOver == true){
+		if (win == true){
+			lose = false;
+			image(congrats, 0, 0);		
+		}
+		if (lose == true){
+			win = false;
+		}
+	}
+}*/
